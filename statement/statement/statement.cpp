15125d19b25fba4bd01e75ea9b7afeb406372d2b
Даниил Ярушин <antoniy274@gmail.com>
2022-05-11 00:25:52 +0300

    fix 11.05.2022

--- statement/statement/statement.cpp
+++ statement/statement/statement.cpp
@@ -5,12 +5,14 @@ int main()
 {
     std::fstream fout;
     fout.open("statement.txt");
-    std::string name, family_name, date;
+    std::string family_name, date;
     std::string max_name, max_family_name;
     int sum = 0, salary = 0, max_salary = 0;
+    char name[20];
     if (fout.is_open()) {
         while (!fout.eof()) {
             fout >> name >> family_name >> salary >> date;
+            if (name[fout.gcount()] == 0) continue;
             sum += salary;
             if (salary > max_salary) {
                 max_salary = salary;
