139b7cf5177bf0a46740c036d7440dd00b83d47e
Даниил Ярушин <antoniy274@gmail.com>
2022-05-17 01:22:11 +0300

    add code

---
+++ rand_picture/rand_picture/rand_picture.cpp
@@ -0,0 +1,25 @@
+﻿#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    std::srand(std::time(nullptr));
+    int height = 0, width = 0;
+    std::cout << "Enter height and width for picture:\n";
+    std::cin >> height >> width;
+
+    std::ofstream file("pic.txt");
+    file.clear();
+    
+    std::vector <int> picture[1][1];
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int a = std::rand() % 2;
+            file << a;
+        }
+        file << "\n";
+    }
+    file.close();
+}
