5af71da5f3a0fcecc9f4c9862e3657b107fcd6dc
Даниил Ярушин <antoniy274@gmail.com>
2022-07-04 00:01:55 +0300

    add code

---
+++ Registry/Registry/Registry.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    map<string, int> patients;
+    string input;
+    int i = 0;
+    while (getline(cin, input)) {
+        if (input == "Next") {
+            auto item = patients.begin();
+            cout << item->first << endl;
+            if (--item->second == 0) patients.erase(patients.begin());
+        }
+        else {
+            if (patients.find(input) == patients.end()) patients.insert(make_pair(input, 1));
+            else patients[input]++;
+        }
+        i++;
+    }
+}
