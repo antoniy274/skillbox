8a9deca64cb4ff9efde79cfb51131d7560cb1a74
Даниил Ярушин <antoniy274@gmail.com>
2022-09-04 16:02:26 +0300

    finish code

--- mobile_phone/mobile_phone/main.cpp
+++ mobile_phone/mobile_phone/main.cpp
@@ -1,11 +1,84 @@
 ﻿// mobile_phone.cpp: определяет точку входа для приложения.
-//
 
 #include "mobile_phone.h"
 #include <iostream>
+#include <string>
+#include <map>
+
+#define ADD_NAME "add"
+#define CALL_NAME "call"
+#define SMS_NAME "sms"
+#define EXIT_NAME "exit"
+
+
+enum Commands {
+    ADD = 1,
+    CALL,
+    SMS,
+    EXIT
+};
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello!!";
-	return 0;
+    int i;
+    MobilePhone phone;
+
+
+    map<string, int> commands;
+    commands[ADD_NAME] = Commands::ADD;
+    commands[CALL_NAME] = Commands::CALL;
+    commands[SMS_NAME] = Commands::SMS;
+    commands[EXIT_NAME] = Commands::EXIT;
+
+    while (true) {
+        string inputCommand;
+
+        cout << "Enter the command:\n";
+        cin >> inputCommand;
+
+        switch (commands[inputCommand]) {
+        case Commands::ADD: {
+            string name;
+            string phoneNumber;
+
+            cout << "Enter name and phone number\n";
+            cin >> name >> phoneNumber;
+
+            while (!phone.isPhoneNumber(phoneNumber)) {
+                cout << "Invalid phone number. Enter valid phone number:\n";
+                cin >> phoneNumber;
+            }
+
+            phone.add(name, phoneNumber);
+            break;
+        }
+        case Commands::CALL: {
+            string value;
+
+            cout << "Enter the name or phone number of contact\n";
+            cin >> value;
+
+            phone.call(value);
+            break;
+        }
+        case Commands::SMS: {
+            string value;
+            string message;
+
+            cout << "Enter the name or phone number of contact and message\n";
+            cin >> value >> message;
+
+            phone.sms(value, message);
+            break;
+        }
+        case Commands::EXIT: {
+            return 0;
+        }
+        default: {
+            cout << "Wrong command.\n";
+        }
+        }
+    }
 }
--- mobile_phone/mobile_phone/mobile_phone.cpp
+++ mobile_phone/mobile_phone/mobile_phone.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <map>
 #include <string>
 #include "mobile_phone.h"
@@ -22,6 +23,18 @@ void Address::setPhoneNumber(std::string value) {
     phone = value;
 }
 
+bool MobilePhone::isPhoneNumber(std::string value) {
+    if (value.length() == 12) {
+        for (int i = 1; i < 12; i++) {
+            if (!isdigit(value[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
 void MobilePhone::add(std::string name, std::string number) {
     Address address;
     address.setName(name);
@@ -30,4 +43,55 @@ void MobilePhone::add(std::string name, std::string number) {
     addressBookNames.insert(std::pair<std::string, Address>(name, address));
     addressBookPhones.insert(std::pair<std::string, Address>(number, address));
 
+}
+
+void MobilePhone::call(std::string value) {
+     //√лобально объ€вить переменную user не получилось, так как не знаю еЄ тип.
+    if (isPhoneNumber(value)) {
+        if (addressBookPhones.find(value) != addressBookPhones.end()) {
+           auto user = addressBookPhones.find(value);
+           std::cout << "CALL to " << user->second.getName() << " on number " << user->second.getPhoneNumber() << std::endl;
+        }
+        else {
+            std::cout << "Invalid phone number\n";
+            return;
+        }
+
+    }
+    else {
+        if (addressBookNames.find(value) != addressBookNames.end()) {
+            auto user = addressBookNames.find(value);
+            std::cout << "CALL to " << user->second.getName() << " on number " << user->second.getPhoneNumber() << std::endl;
+        }
+        else {
+            std::cout << "Invalid phone number\n";
+            return;
+        }
+    }
+}
+
+void MobilePhone::sms(std::string value, std::string message) {
+    if (isPhoneNumber(value)) {
+        if (addressBookPhones.find(value) != addressBookPhones.end()) {
+            auto user = addressBookPhones.find(value);
+            std::cout << "Message: \n" << message << "\n sent to " << user->second.getName()
+                << " on number " << user->second.getPhoneNumber() << std::endl;
+        }
+        else {
+            std::cout << "Invalid phone number\n";
+            return;
+        }
+
+    }
+    else {
+        if (addressBookNames.find(value) != addressBookNames.end()) {
+            auto user = addressBookNames.find(value);
+            std::cout << "Message: \n" << message << "\n sent to " << user->second.getName()
+                << " on number " << user->second.getPhoneNumber() << std::endl;
+        }
+        else {
+            std::cout << "Invalid phone number\n";
+            return;
+        }
+    }
 }
