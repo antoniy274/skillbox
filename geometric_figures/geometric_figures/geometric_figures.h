06486d00cdebff7e7cf207966bb97b77b0668566
Даниил Ярушин <antoniy274@gmail.com>
2022-09-25 02:31:22 +0300

    add code

---
+++ geometric_figures/geometric_figures/geometric_figures.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+enum Collor {
+	RED = 1,
+	BLUE,
+	GREEN,
+	PURPURE,
+	None
+};
+
+struct Coordinate
+{
+	float x = 0.00f;
+	float y = 0.00f;
+};
+
+class Figure {
+private:     
+	Coordinate coordinate;
+	int collor = Collor::None;
+public:
+	virtual float area() {};
+	virtual void outerRectangle() {};
+};
---
+++ geometric_figures/geometric_figures/main.cpp
@@ -0,0 +1,12 @@
+// geometric_figures.cpp: определяет точку входа для приложения.
+//
+
+#include "geometric_figures.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
