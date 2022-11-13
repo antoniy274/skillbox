9216cc7d85ed51dc9c715a6adce631a785ae9818
Даниил Ярушин <antoniy274@gmail.com>
2022-11-13 12:03:27 +0300

    add code

---
+++ lesson_28_1/lesson_28_1/lesson_28_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <thread>
+#include <ctime>
+#include <map>
+
+void hundred_meters(float speed, std::string name);
+
+int main()
+{
+	hundred_meters(15.2, "Felps");
+}
+
+void hundred_meters(float speed, std::string name) {
+	std::time_t t;
+	t = 100 / speed;
+	auto i = 0;
+	float s = 0;
+	while (s <= 100 && i != t)
+	{
+		s += speed * i;
+		std::cout << name << s << "\n";
+		i++;
+	}
+}
