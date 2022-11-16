278a171042ce22bad2b92c73d65ccaacdbd79d97
Даниил Ярушин <antoniy274@gmail.com>
2022-11-16 23:03:03 +0300

    add code

---
+++ example28.1/example28.1/example28.1.cpp
@@ -0,0 +1,42 @@
+﻿#include "example28.1.h"
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <thread>
+#include <mutex>
+
+std::vector<std::string> history;
+std::mutex history_access;
+
+void wait_for_call(int time, std::string name)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(time));
+
+	std::cout << "RING (" << name << ")" << "\n";
+	
+	history_access.lock();
+	history.push_back(name);
+	history_access.unlock();
+}
+
+int main()
+{
+	int time;
+	std::cin >> time;
+	std::string name;
+	std::cin >> name;
+	std::thread call(wait_for_call, time, name);
+	std::thread call_skillbox(wait_for_call, 4, "Skillbox");
+	call.detach();
+	call_skillbox.detach();
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	if (call.joinable()) call.join();
+	if (call_skillbox.joinable())call_skillbox.join();
+
+	std::cout << "Missed calls:\n";
+	history_access.lock();
+	for (int i = 0; i < history.size(); ++i) {
+		std::cout << history[i] << " ";
+	}
+	history_access.unlock();
+}
--- lesson_28_1/lesson_28_1/lesson_28_1.cpp
+++ lesson_28_1/lesson_28_1/lesson_28_1.cpp
@@ -1,24 +1,67 @@
 ﻿#include <iostream>
 #include <thread>
 #include <ctime>
+#include <vector>
 #include <map>
 
-void hundred_meters(float speed, std::string name);
+void hundred_meters(double speed, std::string name);
+std::map<double, std::string> list_swimmer;
+
+struct Swimmer
+{
+	std::string name;
+	int speed;
+};
 
 int main()
 {
-	hundred_meters(15.2, "Felps");
+	std::string name;
+	int speed;
+	std::cout << "Enter swimming name: ";
+	std::cin >> name;
+	std::cout << "Enter swimming speed: ";
+	std::cin >> speed;
+	std::thread firstTrack(speed, name);
+
+	std::cout << "Enter swimming name: ";
+	std::cin >> name;
+	std::cout << "Enter swimming speed: ";
+	std::cin >> speed;
+	std::thread secondTrack(speed, name);
+
+	std::cout << "Enter swimming name: ";
+	std::cin >> name;
+	std::cout << "Enter swimming speed: ";
+	std::cin >> speed;
+	std::thread thirdTrack(speed, name);
+
+	std::cout << "Enter swimming name: ";
+	std::cin >> name;
+	std::cout << "Enter swimming speed: ";
+	std::cin >> speed;
+	std::thread fourTrack(speed, name);
+
+	firstTrack.join();
+	secondTrack.join();
+	thirdTrack.join();
+	fourTrack.join();
 }
 
-void hundred_meters(float speed, std::string name) {
-	std::time_t t;
+void hundred_meters(double speed, std::string name) {
+	double t;
 	t = 100 / speed;
 	auto i = 0;
-	float s = 0;
+	double s = speed;
 	while (s <= 100 && i != t)
 	{
-		s += speed * i;
-		std::cout << name << s << "\n";
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::cout << name << " " << s << "\n";
+		s += speed;
 		i++;
 	}
+	s -= speed;
+	s += ((t - (int)t) * speed);
+	std::cout << name << " " << s << "\n";
+	std::pair<int, std::string> sw;
+	list_swimmer.insert(sw);
 }
