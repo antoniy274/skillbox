4efbac50e9c256f51a71ffe37f6d880a0f83e09d
Даниил Ярушин <antoniy274@gmail.com>
2022-11-18 22:26:35 +0300

    add code 18.11

--- lesson_28_1/lesson_28_1/lesson_28_1.cpp
+++ lesson_28_1/lesson_28_1/lesson_28_1.cpp
@@ -1,53 +1,55 @@
 ﻿#include <iostream>
+#include <algorithm>
 #include <thread>
 #include <ctime>
 #include <vector>
 #include <map>
+#include <mutex>
 
-void hundred_meters(double speed, std::string name);
+void hundred_meters(std::string name, double speed);
 std::map<double, std::string> list_swimmer;
+std::mutex dashboard_swimmer;
 
 struct Swimmer
 {
 	std::string name;
-	int speed;
+	double speed;
+	double time = 100 / speed;
 };
+
+void sortirovka(std::vector<Swimmer> swimmer);
 
 int main()
 {
-	std::string name;
-	int speed;
-	std::cout << "Enter swimming name: ";
-	std::cin >> name;
-	std::cout << "Enter swimming speed: ";
-	std::cin >> speed;
-	std::thread firstTrack(speed, name);
+	std::vector<Swimmer> swimmer;
+	for (int i = 0; i < 6; ++i) {
+		Swimmer example;
+		std::cin >> example.name >> example.speed;
+		swimmer.push_back(example);
+	}
 
-	std::cout << "Enter swimming name: ";
-	std::cin >> name;
-	std::cout << "Enter swimming speed: ";
-	std::cin >> speed;
-	std::thread secondTrack(speed, name);
-
-	std::cout << "Enter swimming name: ";
-	std::cin >> name;
-	std::cout << "Enter swimming speed: ";
-	std::cin >> speed;
-	std::thread thirdTrack(speed, name);
-
-	std::cout << "Enter swimming name: ";
-	std::cin >> name;
-	std::cout << "Enter swimming speed: ";
-	std::cin >> speed;
-	std::thread fourTrack(speed, name);
+	std::thread firstTrack(hundred_meters, swimmer[0].name, swimmer[0].speed);
+	std::thread secondTrack(hundred_meters, swimmer[1].name, swimmer[1].speed);
+	std::thread thirdTrack(hundred_meters, swimmer[2].name, swimmer[2].speed);
+	std::thread fourTrack(hundred_meters, swimmer[3].name, swimmer[3].speed);
+	std::thread fiveTrack(hundred_meters, swimmer[4].name, swimmer[4].speed);
+	std::thread sixTrack(hundred_meters, swimmer[5].name, swimmer[5].speed);
 
 	firstTrack.join();
 	secondTrack.join();
 	thirdTrack.join();
 	fourTrack.join();
+	fiveTrack.join();
+	sixTrack.join();
+
+	sortirovka(swimmer);
+
+	for (int i = 0; i < swimmer.size(); ++i) {
+		std::cout << i + 1 << " Name: " << swimmer[i].name << " Time " << swimmer[i].time << "\n";
+	}
 }
 
-void hundred_meters(double speed, std::string name) {
+void hundred_meters(std::string name, double speed) {
 	double t;
 	t = 100 / speed;
 	auto i = 0;
@@ -62,6 +64,26 @@ void hundred_meters(double speed, std::string name) {
 	s -= speed;
 	s += ((t - (int)t) * speed);
 	std::cout << name << " " << s << "\n";
-	std::pair<int, std::string> sw;
-	//list_swimmer.insert(sw);
+	std::pair<int, std::string> sw(t, name);
+	//dashboard_swimmer.lock();
+	//for (int i = 0; i < 6; ++i) {
+		//swimmer[i].time = t;
+	//}
+	//dashboard_swimmer.unlock();
+}
+
+void sortirovka(std::vector<Swimmer> swimmer) {
+	for (int i = 0; i < swimmer.size(); ++i){
+		bool flag = false;
+		for (int j = 0; j < swimmer.size() - i; ++j) {
+			if (swimmer[j].time > swimmer[j + 1].time) {
+				auto temp = swimmer[j].time;
+				swimmer[j].time = swimmer[j + 1].time;
+				swimmer[j + 1].time = temp;
+				flag = true;
+			}
+			if (flag == false)
+				break;
+		}
+	}
 }
