833267b95cee7c4052a2b35daa191fa11194479f
Даниил Ярушин <antoniy274@gmail.com>
2023-03-19 13:47:02 +0300

    add code

---
+++ lesson30.1/lesson30.1/lesson30.1.cpp
@@ -0,0 +1,10 @@
+﻿#include "lesson30.1.h"
+#include "iostream"
+#include "cpr/cpr.h"
+
+
+int main()
+{
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+	std::cout << r.text << "\n";
+}
