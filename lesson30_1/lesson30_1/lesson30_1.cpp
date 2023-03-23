2b4bff0d93678bf129c9944aa5828149e758dbc5
Даниил Ярушин <antoniy274@gmail.com>
2023-03-23 22:44:39 +0300

    add code

--- lesson30_1/lesson30_1/lesson30_1.cpp
+++ lesson30_1/lesson30_1/lesson30_1.cpp
@@ -1,13 +1,39 @@
-﻿// lesson30_1.cpp: определяет точку входа для приложения.
-//
-
-#include "lesson30_1.h"
-#include "iostream"
+﻿#include "iostream"
 #include "cpr/cpr.h"
 
 
 int main()
 {
-	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-	std::cout << r.text << "\n";
+	std::string str;
+	
+	std::cout << "Enter the commands: <<get>>, <<post>>, <<put>>, <<delete>>, <<patch>>"
+		<< " or enter the command << exit >> to exit" << "\n";
+
+	while (std::getline(std::cin, str) && str != "exit") {
+		
+		cpr::Response r;
+		if (str == "get") {
+			r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+			std::cout << r.text << "\n";
+		}
+		else if (str == "post") {
+			r = cpr::Post(cpr::Url("http://httpbin.org/post"));
+			std::cout << r.text << "\n";
+		}
+		else if (str == "put") {
+			r = cpr::Put(cpr::Url("http://httpbin.org/put"));
+			std::cout << r.text << "\n";
+		}
+		else if (str == "delete") {
+			r = cpr::Put(cpr::Url("http://httpbin.org/delete"));
+			std::cout << r.text << "\n";
+		}
+		else if (str == "patch") {
+			r = cpr::Put(cpr::Url("http://httpbin.org/patch"));
+			std::cout << r.text << "\n";
+		}
+
+		std::cout << "Enter the commands: <<get>>, <<post>>, <<put>>, <<delete>>, <<patch>>" 
+			<<" or enter the command << exit >> to exit" << "\n";
+	}
 }
