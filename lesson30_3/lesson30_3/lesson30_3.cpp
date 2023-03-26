052613a5d79326018b852924ee123c881f6f95b8
Даниил Ярушин <antoniy274@gmail.com>
2023-03-26 22:39:18 +0300

    add code

---
+++ lesson30_3/lesson30_3/lesson30_3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "cpr/cpr.h"
+
+
+int main()
+{
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get/?foo=first&animal=cat&bar=second"));
+	std::cout << r.text << "\n";
+
+	std::string name, city;
+	std::cin >> name >> city;
+	cpr::Response rp = cpr::Post(cpr::Url("http://httpbin.org/post"), cpr::Payload({ {"name", name.c_str()},
+																					{"city",city.c_str()} }));
+	std::cout << rp.text << "\n";
+}
