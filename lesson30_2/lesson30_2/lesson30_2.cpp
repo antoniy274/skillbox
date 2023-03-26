da1f728e32bd39d009be7b2f5deea0f1db170047
Даниил Ярушин <antoniy274@gmail.com>
2023-03-26 18:49:40 +0300

    finish coding

--- lesson30_2/lesson30_2/lesson30_2.cpp
+++ lesson30_2/lesson30_2/lesson30_2.cpp
@@ -4,6 +4,9 @@
 int main()
 {
 	cpr::Response r;
-	r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-	std::cout << r.text << "\n";
+	r = cpr::Get(cpr::Url("http://httpbin.org/html"), cpr::Header{ {"accept", "text/html"} });
+	std::string str = "<h1>";
+	auto begin = r.text.find("<h1>") + str.size();
+	auto end = r.text.find("</h1>", begin - 1) - begin;
+	std::cout << r.text.substr(begin, end) << "\n";
 }
