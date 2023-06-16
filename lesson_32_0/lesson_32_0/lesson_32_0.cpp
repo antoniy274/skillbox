da39f8f84b154c60f83809e0b08fd79ffc34eeb9
Даниил Ярушин <antoniy274@gmail.com>
2023-06-16 11:07:30 +0300

    add code

---
+++ lesson_32_0/lesson_32_0/lesson_32_0.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+struct Record
+{
+	std::string name;
+	std::string family;
+	int age;
+};
+
+int main()
+{
+	Record record;
+	std::cin >> record.name >> record.family >> record.age;
+	std::ofstream file("record.json");
+	file << "{" << "\"name\": \"" << record.name << "\", \"family\": \"" << record.family << "\"}" << std::endl;
+}
