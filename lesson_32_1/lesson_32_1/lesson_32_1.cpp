40911a6b4f1956de06b99204cd8de5f7f02080a5
Даниил Ярушин <antoniy274@gmail.com>
2023-06-19 00:10:59 +0300

    finish code

---
+++ lesson_32_1/lesson_32_1/lesson_32_1.cpp
@@ -0,0 +1,12 @@
+﻿// lesson_32_1.cpp: определяет точку входа для приложения.
+//
+
+#include "lesson_32_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
