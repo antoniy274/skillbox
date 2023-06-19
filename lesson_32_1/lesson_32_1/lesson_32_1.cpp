af2a7d171995558905004708bf7bf8d37d4d1134
Даниил Ярушин <antoniy274@gmail.com>
2023-06-20 00:12:27 +0300

    add code

--- lesson_32_1/lesson_32_1/lesson_32_1.cpp
+++ lesson_32_1/lesson_32_1/lesson_32_1.cpp
@@ -1,12 +1,41 @@
-﻿// lesson_32_1.cpp: определяет точку входа для приложения.
-//
+﻿#include<iostream>
+#include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
 
-#include "lesson_32_1.h"
-
-using namespace std;
+struct Film
+{
+	std::string name;
+	std::string director;
+	std::string writer;
+	std::string actors;
+	std::string producers;
+	std::string date;
+	std::string country;
+	std::string studio;
+};
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	Film film;
+	std::ifstream file("film.json");
+	nlohmann::json dict;
+	file >> dict;
+	film.name = dict["Original title"];
+	film.director = dict["Director"];
+	film.writer = dict["Writer"];
+	film.actors = dict["Actors"];
+	film.producers = dict["Producers"];
+	film.date = dict["World Premiere"];
+	film.country = dict["Country"];
+	film.studio = dict["Studio"];
+
+	std::cout << film.name << std::endl;
+	std::cout << film.director << std::endl;
+	std::cout << film.writer << std::endl;
+	std::cout << film.actors << std::endl;
+	std::cout << film.producers << std::endl;
+	std::cout << film.date << std::endl;
+	std::cout << film.country << std::endl;
+	std::cout << film.studio << std::endl;
 }
