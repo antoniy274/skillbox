0a8e5bbc9fac8d7043b9ef9b10532f40d5c716a3
Даниил Ярушин <antoniy274@gmail.com>
2023-06-21 23:36:34 +0300

    add

---
+++ lesson_32_2/lesson_32_2/lesson_32_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+int main()
+{
+    std::ifstream file("C:/Users/anton/Desktop/skillbox/lesson_32_2/bin/films.json");
+
+    nlohmann::json film;
+    file >> film;
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open file\n";
+        throw 1;
+    }
+    file.close();
+}
