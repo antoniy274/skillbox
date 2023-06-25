88d5203ace5a54aa20dd9a31fa69ee85002bd69f
Даниил Ярушин <antoniy274@gmail.com>
2023-06-26 00:38:04 +0300

    add code

--- lesson_32_2/lesson_32_2/lesson_32_2.cpp
+++ lesson_32_2/lesson_32_2/lesson_32_2.cpp
@@ -1,19 +1,46 @@
 ﻿#include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
 #include "nlohmann/json.hpp"
 
 int main()
 {
-    std::ifstream file("C:/Users/anton/Desktop/skillbox/lesson_32_2/bin/films.json");
+	std::ifstream file("C:/Users/anton/Desktop/skillbox/lesson_32_2/lesson_32_2/films.json");
+	nlohmann::json dict;
+	file >> dict;
 
-    nlohmann::json film;
-    file >> film;
-    if (!file.is_open())
+	std::string name;
+	std::cout << "Enter actors name: ";
+	std::getline(std::cin, name);
+
+    std::vector<std::string> f;
+
+    for (auto it = dict.begin(); it != dict.end(); ++it)
     {
-        std::cerr << "Unable to open file\n";
-        throw 1;
+        auto act = it->find("Actors");
+
+        if (act != it->end())
+        {
+            auto na = act->find(name);
+            if (na != act->end())
+            {
+                std::string out = "Film: " + it.key()
+                    + ", role in the film : " + nlohmann::to_string(na.value());
+                f.push_back(out);
+            }
+
+        }
     }
-    file.close();
+
+    if (f.size() > 0)
+    {
+        for (auto& x : f)
+            std::cout << x << std::endl;
+    }
+    else
+    {
+        std::cout << "Sorry, no such actor has been found";
+    }
+
+	file.close();
 }
