0d5e2b43f95d30c85b795a973d1ec2c3dc091835
Даниил Ярушин <antoniy274@gmail.com>
2023-07-11 00:08:21 +0300

    add code

---
+++ lesson_33_3/lesson_33_3/Registry.cpp
@@ -0,0 +1,2 @@
+﻿#include "Registry.h"
+
---
+++ lesson_33_3/lesson_33_3/Registry.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <vector>
+
+
+template <typename T1, typename T2>
+class Registry:
+{
+	template <class K, class V>
+	struct Pair
+	{
+		K key;
+		V value;
+		Pair() : kkey(K()), vvalue(V()) {}
+	};
+	std::vector<Pair<T1, T2>> reg;
+public:
+	void add(T1 first, T2 second);
+	void remove(T1 first);
+	void print();
+	T2 find(T1 first);
+};
+
+template <typename T1, typename T2>
+void Registry<T1,T2>::add(T1 first, T2 second)
+{
+	Pair<T1, T2> pair;
+	pair.key = first;
+	pair.value = second;
+	reg.push_back(pair);
+}
+
+template <typename T1, typename T2>
+void Registry<T1, T2>::remove(T1 first)
+{
+	if (reg.empty())
+		std::count << "Register is empty";
+
+	int s = 0;
+	while (s <= reg.size())
+	{
+		if (reg == first)
+		{
+			reg.erase(reg.begin() + s);
+		}
+		s++;
+	}
+}
