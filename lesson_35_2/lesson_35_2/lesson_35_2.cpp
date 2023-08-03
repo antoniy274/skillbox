338650d362ec77a63dbe9159ec52433ab9bb6cda
Даниил Ярушин <antoniy274@gmail.com>
2023-08-03 23:25:03 +0300

    add code

---
+++ lesson_35_2/lesson_35_2/lesson_35_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <unordered_set>
+
+int main()
+{
+    std::vector<int> vec = { 1, 2, 3, 3, 4, 4, 1, 19, 23, 19 };
+
+    auto set = [](const std::vector<int>& vec)
+    {
+        std::unordered_set<int> u_set(vec.begin(), vec.end());
+        return std::make_unique<std::vector<int>>(u_set.begin(), u_set.end());
+    };
+
+    auto ptr = set(vec);
+
+    for (const auto& s : *ptr.get())
+        std::cout << s << " ";
+    std::cout << std::endl;
+
+
+}
